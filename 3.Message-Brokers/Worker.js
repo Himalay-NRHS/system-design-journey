a647275c0d0b90cb0e58616649c26f20d5c59306
Himalay N <157269880+Himalay-NRHS@users.noreply.github.com>
2025-12-21 12:40:19 +0530

    Modify worker to simulate slow email processing
    Updated worker to simulate slow processing with a delay.

--- 3.Message-Brokers/Worker.js
+++ 3.Message-Brokers/Worker.js
@@ -4,28 +4,14 @@ const connection = require("./redis");
 
 // Create a worker
 const worker = new Worker(
-  "email-queue",             // same queue name
+  "email-queue",        // same queue name
   async (job) => {
     console.log("Processing job:", job.id);
     console.log("Data:", job.data);
 
-    // simulate work
-    if (Math.random() < 0.5) {
-      throw new Error("Random failure");
-    }
+    // simulate slow work
+    await new Promise((res) => setTimeout(res, 3000));
 
-    return "Email sent";
-  },
-  {
-    connection,
-    concurrency: 5            // 5 jobs in parallel
-  }
+    console.log("Email sent");
+  }, { connection}
 );
-
-worker.on("completed", (job) => {
-  console.log(`Job ${job.id} completed`);
-});
-
-worker.on("failed", (job, err) => {
-  console.log(`Job ${job.id} failed:`, err.message);
-});
